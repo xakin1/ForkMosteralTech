096118651e7ac559915acf2d6503ee4c1272cb89
xaquín <xaquin.perez1@udc.es>
2023-05-04 13:16:20 +0200

    arreglada la configuración de la base de datos

--- Backend/server/refresh-dev-db.sql
+++ Backend/server/refresh-dev-db.sql
@@ -1,15 +1,36 @@
-SELECT pg_terminate_backend(pg_stat_activity.pid)
-FROM pg_stat_activity
-WHERE pg_stat_activity.datname = 'gema-dev'
-AND pid <> pg_backend_pid();
 
-DROP DATABASE IF EXISTS "gema-dev";
 
-SELECT pg_terminate_backend(pg_stat_activity.pid)
-FROM pg_stat_activity
-WHERE pg_stat_activity.datname = 'gema'
-AND pid <> pg_backend_pid();
+CREATE TABLE IF NOT EXISTS AppUser (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  surname VARCHAR(50),
+  location VARCHAR(50)
+);
 
-CREATE DATABASE "gema-dev"
-WITH TEMPLATE gema
-OWNER postgres;
+CREATE TYPE IF NOT EXISTS State AS ENUM ('new', 'almost new', 'second hand');
+
+
+CREATE TABLE IF NOT EXISTS Product (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  location VARCHAR(50),
+  state State,
+  user_id INTEGER REFERENCES AppUser(id)
+);
+
+CREATE TYPE IF NOT EXISTS ActionType AS ENUM ('purchase', 'sale', 'rent');
+
+CREATE TABLE IF NOT EXISTS Transactions (
+  id SERIAL PRIMARY KEY,
+  date TIMESTAMP,
+  action ActionType,
+  user_id INTEGER REFERENCES AppUser(id),
+  product_id INTEGER REFERENCES Product(id)
+);
+
+CREATE TABLE IF NOT EXISTS Favourites (
+  id SERIAL PRIMARY KEY,
+  date TIMESTAMP,
+  user_id INTEGER REFERENCES AppUser(id),
+  product_id INTEGER REFERENCES Product(id)
+);
